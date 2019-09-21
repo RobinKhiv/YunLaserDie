81ca502415d9a1f81ae19d68374c9da10fb1f0f9
Robin Khiv <robinkhiv@gmail.com>
2019-09-21 15:38:18 -0700

    created header and footer templates

---
+++ public_html/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Yun Laser Die LLC</title>
+</head>
+<body>
+    <?php include("../resources/templates/header.php"); ?>
+</body>
+</html>
---
+++ resources/templates/header.php
@@ -0,0 +1,12 @@
+<header>
+    <nav role="navigation">
+        <div class="brand">
+            <a href="">Yun Laser Die</a>
+        </div>
+        <div class="nav-text right">
+            <a href="">About Us</a>
+            <a href="">Services</a>
+            <a href="">Contact</a>
+        </div>
+    </nav>
+</header>
