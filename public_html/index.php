449fd18d4dfb6d235efedb07f913288469d5d7eb
Robin Khiv <robinkhiv@gmail.com>
2019-09-21 16:18:58 -0700

    added page routes to index

--- public_html/index.php
+++ public_html/index.php
@@ -5,6 +5,23 @@
     <title>Yun Laser Die LLC</title>
 </head>
 <body>
-    <?php include("../resources/templates/header.php"); ?>
+    <?php include("../resources/components/header.php"); ?>
+    <?php
+    isset($_GET['route']) ? $route =$_GET['route'] : $route = 'home';
+    switch ($route){
+        case 'home':
+            include('./routes/home.php');
+            break;
+        case 'about':
+            include('./routes/about.php');
+            break;
+        case 'services':
+            include('./routes/services.php');
+            break;
+        case 'contact';
+            include('./routes/contact.php');
+            break;
+    }
+    ?>
 </body>
 </html>
---
+++ resources/components/header.php
@@ -0,0 +1,12 @@
+<header>
+    <nav role="navigation">
+        <div class="brand">
+            <a href="index.php/?route=home">Yun Laser Die</a>
+        </div>
+        <div class="nav-text right">
+            <a href="index.php/?route=about">About Us</a>
+            <a href="index.php/?route=services">Services</a>
+            <a href="index.php/?route=contact">Contact</a>
+        </div>
+    </nav>
+</header>
