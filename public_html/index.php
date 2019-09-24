3f559ecb65d3b6ce89ff543117bb73314941bf03
Robin Khiv <robinkhiv@gmail.com>
2019-09-23 21:37:00 -0700

    added css for header

--- public_html/index.php
+++ public_html/index.php
@@ -1,13 +1,14 @@
+<?php  isset($_GET['route']) ? $route =$_GET['route'] : $route = 'home'; ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <title>Yun Laser Die LLC</title>
+    <link  rel="stylesheet" type="text/css" href="./css/header.css">
 </head>
 <body>
     <?php include('../resources/components/header.php'); ?>
     <?php
-    isset($_GET['route']) ? $route =$_GET['route'] : $route = 'home';
     switch ($route){
         case 'home':
             include('../resources/routes/home.php');
--- resources/components/header.php
+++ resources/components/header.php
@@ -1,12 +1,12 @@
 <header>
     <nav role="navigation">
-        <div class="brand">
-            <a href="index.php/?route=home">Yun Laser Die</a>
+        <div class="brand nav-text-left">
+            <a class="nav-text" href="index.php?route=home">Yun Laser Die</a>
         </div>
-        <div class="nav-text right">
-            <a href="index.php?route=about">About Us</a>
-            <a href="index.php?route=services">Services</a>
-            <a href="index.php?route=contact">Contact</a>
+        <div class="nav-text-right">
+            <a class="nav-text" href="index.php?route=about">About Us</a>
+            <a class="nav-text" href="index.php?route=services">Services</a>
+            <a class="nav-text" href="index.php?route=contact">Contact</a>
         </div>
     </nav>
 </header>
