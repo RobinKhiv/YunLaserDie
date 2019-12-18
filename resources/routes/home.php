0ff5f5b8c7a5f71762441128ebd067dfd7adbfa2
Robin Khiv <robinkhiv@gmail.com>
2019-12-17 21:59:28 -0800

    added content to homepage

--- resources/routes/home.php
+++ resources/routes/home.php
@@ -1,1 +1,36 @@
-<p>home</p>
+<section>
+    <div>
+        <h2>Never Have a problem with Die Cutting Agian</h2>
+        <p>Die Cutting Companies rely on Yun Laser Die to delivery quality die cut tools that meet strict specifications</p> 
+    </div>
+    <div>
+        <img src="" alt="">
+        <p>Laser Cut Precision</p>
+    </div>
+    <div>
+        <img src="" alt="">
+        <p>Close Tolerance Guarenteed</p>
+    </div>
+    <div>
+        <img src="" alt="">
+        <p>Over 30 years Experience</p>
+    </div>
+</section>
+<section>
+    <div>
+        <img src="" alt="">
+        <div>
+            <h2>Die Cut Tools</h2>
+            <p></p>
+            <button>Learn More</button>
+        </div>
+    </div>
+    <div>
+        <img src="" alt="">
+        <div>
+            <h2>Laser Cut Services</h2>
+            <p></p>
+            <button>Learn More</button>
+        </div>
+    </div>
+</section>
